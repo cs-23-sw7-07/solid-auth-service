3da49f51b0f22a1f8e813edfcd88eedd040941f4
S3JER <72788875+S3JER@users.noreply.github.com>
2023-11-23 10:05:00 +0100

    createDataAuthorization

--- src/builder/authorization-builder.ts
+++ src/builder/authorization-builder.ts
@@ -1,52 +1,69 @@
 import {
-  AccessAuthorization,
-  Agent,
-  DataAuthorization,
+    AccessAuthorization,
+    Agent,
+    DataAuthorization,
+    DataRegistration,
 } from "solid-interoperability";
 import { AuthorizationAgent } from "../authorization-agent";
 import { DataAccessScope } from "../application/data-access-scope";
 import { AccessNeedGroup } from "../application/access-need-group";
+import { forEachChild } from "typescript";
 
 export class AuthorizationBuilder {
-  private data_authorizations: Map<string, DataAuthorization> = new Map<
-    string,
-    DataAuthorization
-  >();
-  private access_authorizations!: AccessAuthorization;
+    private data_authorizations: Map<string, DataAuthorization> = new Map<
+        string,
+        DataAuthorization
+    >();
+    private access_authorizations!: AccessAuthorization;
 
-  constructor(
-    public authorizationAgent: AuthorizationAgent,
-    public grantee: Agent,
-  ) {}
+    constructor(
+        public authorizationAgent: AuthorizationAgent,
+        public grantee: Agent,
+    ) { }
 
-  generateId(): string {
-    return this.authorizationAgent.generateId(
-      this.authorizationAgent.AuthorizationRegistry_container,
-    );
-  }
+    generateId(): string {
+        return this.authorizationAgent.generateId(
+            this.authorizationAgent.AuthorizationRegistry_container,
+        );
+    }
 
-  async createDataAuthorization(dataAccessScope: DataAccessScope) {
-    const data_authorizations = await dataAccessScope.toDataAuthoization(this);
-    this.data_authorizations.set(
-      dataAccessScope.accessNeed.uri,
-      data_authorizations,
-    );
-  }
+    async createDataAuthorization(dataAccessScope: DataAccessScope) {
+        const shapeTree = dataAccessScope.accessNeed.getRegisteredShapeTree();
 
-  getCreatedDataAuthorizations(): DataAuthorization[] {
-    return Array.from(this.data_authorizations.values());
-  }
+        const dataRegs: DataRegistration[] = await this.authorizationAgent.getAllRegistrations();
 
-  async createAccessAuthorization(access_need_group: AccessNeedGroup) {
-    this.access_authorizations = await access_need_group.toAccessAuthorization(
-      this.generateId(),
-      this.authorizationAgent,
-      this.grantee,
-      this.getCreatedDataAuthorizations(),
-    );
-  }
+        const exists = dataRegs.some(dataReg => dataReg.registeredShapeTree === shapeTree);
+        if (!exists) {
+            new DataRegistration(
+                this.authorizationAgent.generateId(this.authorizationAgent.DataRegistry_container),
+                this.authorizationAgent.social_agent,
+                this.authorizationAgent.authorization_agent,
+                new Date(),
+                new Date(),
+                shapeTree
+            );
+        }
+        const data_authorization = await dataAccessScope.toDataAuthoization(this);
+            this.data_authorizations.set(
+                dataAccessScope.accessNeed.uri,
+                data_authorization,
+            );
+    }
 
-  getCreatedAccessAuthorization(): AccessAuthorization {
-    return this.access_authorizations!;
-  }
+    getCreatedDataAuthorizations(): DataAuthorization[] {
+        return Array.from(this.data_authorizations.values());
+    }
+
+    async createAccessAuthorization(access_need_group: AccessNeedGroup) {
+        this.access_authorizations = await access_need_group.toAccessAuthorization(
+            this.generateId(),
+            this.authorizationAgent,
+            this.grantee,
+            this.getCreatedDataAuthorizations(),
+        );
+    }
+
+    getCreatedAccessAuthorization(): AccessAuthorization {
+        return this.access_authorizations!;
+    }
 }

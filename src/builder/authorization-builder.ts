f5a9f84ad8d52b56c6897a5e13770577e7cdb20c
S3JER <72788875+S3JER@users.noreply.github.com>
2023-11-23 10:18:01 +0100

    updated createDataAuthorization

--- src/builder/authorization-builder.ts
+++ src/builder/authorization-builder.ts
@@ -3,11 +3,14 @@ import {
     Agent,
     DataAuthorization,
     DataRegistration,
+    RdfFactory,
 } from "solid-interoperability";
 import { AuthorizationAgent } from "../authorization-agent";
 import { DataAccessScope } from "../application/data-access-scope";
 import { AccessNeedGroup } from "../application/access-need-group";
 import { forEachChild } from "typescript";
+import { createContainer, updateContainerResource } from "../utils/modify-pod";
+import { parseTurtle } from "../utils/turtle-parser";
 
 export class AuthorizationBuilder {
     private data_authorizations: Map<string, DataAuthorization> = new Map<
@@ -29,12 +32,11 @@ export class AuthorizationBuilder {
 
     async createDataAuthorization(dataAccessScope: DataAccessScope) {
         const shapeTree = dataAccessScope.accessNeed.getRegisteredShapeTree();
-
         const dataRegs: DataRegistration[] = await this.authorizationAgent.getAllRegistrations();
-
         const exists = dataRegs.some(dataReg => dataReg.registeredShapeTree === shapeTree);
+
         if (!exists) {
-            new DataRegistration(
+            const dataReg = new DataRegistration(
                 this.authorizationAgent.generateId(this.authorizationAgent.DataRegistry_container),
                 this.authorizationAgent.social_agent,
                 this.authorizationAgent.authorization_agent,
@@ -42,12 +44,18 @@ export class AuthorizationBuilder {
                 new Date(),
                 shapeTree
             );
+            const fetch = this.authorizationAgent.session.fetch;
+            createContainer(fetch, dataReg.id);
+            const container_iri = dataReg.id + '.meta';
+            const dataset: string = await new RdfFactory().create(dataReg) as string;
+            const serializedDataset = (await parseTurtle(dataset, dataReg.id)).dataset;
+            updateContainerResource(this.authorizationAgent.session, container_iri, serializedDataset);
         }
         const data_authorization = await dataAccessScope.toDataAuthoization(this);
-            this.data_authorizations.set(
-                dataAccessScope.accessNeed.uri,
-                data_authorization,
-            );
+        this.data_authorizations.set(
+            dataAccessScope.accessNeed.uri,
+            data_authorization,
+        );
     }
 
     getCreatedDataAuthorizations(): DataAuthorization[] {
